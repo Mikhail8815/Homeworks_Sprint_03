bac209ee93a9302fcb473517fc0973da76fabf6d
Mikhail8815 <mishutka150588@mail.ru>
2025-01-20 20:53:29 +0300

    SP_03_WEEK_2

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,7 +19,10 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        const savedValue = restoreState<string>('hw6-editable-span-value', '');
+        if (savedValue) {
+            setValue(savedValue);
+        }
     }
 
     return (
