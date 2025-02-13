073b0462965c2bf88adfa977a926158e8238b68c
Mikhail8815 <mishutka150588@mail.ru>
2025-02-13 18:58:39 +0300

    SP_03_WEEK_4

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,22 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': { //
+            const sortedState = [...state]
+            sortedState.sort((a, b) => {
+                if (action.payload === 'up') {
+                    return a.name.localeCompare(b.name); // Сортировка по имени по возрастанию
+                } else {
+                    return b.name.localeCompare(a.name); // Сортировка по имени по убыванию
+                }
+            })
+            return sortedState // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >= 18) // need to fix
         }
         default:
             return state
